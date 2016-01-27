d6de252f16566c2d10afddfdde2b836e8a530ad2
Stuart Urback <stuart@urback.net>
2016-01-26 17:40:37 -0700

    Complete sign-up and sign-in forms

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signin', function(req, res, next){
+  res.render('signin')
+})
+
+router.get('/signup', function(req, res, next){
+  res.render('signup')
+})
+
 module.exports = router;
