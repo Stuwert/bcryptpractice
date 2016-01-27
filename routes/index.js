a6f219bdf0aabeb1d88895eda26bf2f8204d3c15
Stuart Urback <stuart@urback.net>
2016-01-27 15:47:21 -0700

    Complete exercise

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
 var router = express.Router();
 var validation = require('../controls/validations')
+var knex = require('../db/knex')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { email: req.cookies.user});
 });
 
 router.get('/signin', function(req, res, next){
@@ -20,10 +22,35 @@ router.post('/signup', function(req, res, next){
   var password = validation.passwordIsNotBlank(req.body.password || "")
   var confirm = req.body.password === req.body.confirm ? true : "Your passwords need to match"
   if(email.bool === true && password.bool === true && confirm == true){
-    res.send("It worked!")
+    var hash = bcrypt.hashSync(req.body.password, 10);
+    Users().insert({email: req.body.email, hallpass: hash}).then(function(){
+      res.cookie('user', req.body.email)
+      res.redirect('/')
+    })
   }else{
     res.render('signup', {email: email, password: password, confirm: confirm})
   }
 })
+
+router.post('/signin', function(req, res, next){
+  var email = validation.emailIsValid(req.body.email || "");
+  var password = validation.passwordIsNotBlank(req.body.password || "")
+  if(email.bool === true && password.bool === true){
+    Users().where('email', req.body.email).first().then(function(person){
+      if(bcrypt.compareSync(req.body.password, person.hallpass)){
+        res.send("You're In")
+      }else{
+        res.send('Wrong fucking password')
+      };
+    })
+  }else{
+    res.render('signin', {email: email, password: password})
+  }
+})
+
+function Users(){
+  return knex('users');
+}
+
 
 module.exports = router;
